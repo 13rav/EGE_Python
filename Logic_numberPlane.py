526bca3fca16e7ad9102159d7bf3e80dffdac60c
Andrey <13rav.pr@gmail.com>
2022-11-21 20:37:54 +0300

    cew

--- Logic_numberPlane.py
+++ Logic_numberPlane.py
@@ -3,7 +3,7 @@ for A in range (0, 300):
 
     for x in range (0, 300):
         for y in range (0, 300):
-            if ((4*x + 3*y < A) or (x > y) or (y > 13)):
+            if ((2*x + 3*y < A) or (x > y) or (y > 24)):
 
                 c += 1
     if c == 90000:
